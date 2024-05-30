add1fb458f1dc949db8725016722c8da8005dae9
Hansel <hansel.hir@gmail.com>
2024-05-30 10:49:39 -0500

    change rename group-id

--- main.tf
+++ main.tf
@@ -134,7 +134,7 @@ resource "aws_lb_listener" "ecs_listener" {
   protocol           = "HTTP"
   default_action {
     type             = "forward"
-    target_group_arn = "aws_lb_target_group"
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
   }
 }
 
