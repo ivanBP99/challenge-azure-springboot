657662fbd7250a9c73029e89be28a9734597d592
Hansel <hansel.hir@gmail.com>
2024-05-30 00:43:19 -0500

    two subnet

--- main.tf
+++ main.tf
@@ -56,6 +56,28 @@ provider "aws" {
 # RESOURCES
 #############################################################################
 
+resource "aws_vpc" "main" {
+  cidr_block = "var.vpc_cidr"
+  enable_dns_hostnames = true
+  tags = {
+    name = "main"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "cidrsubnet(aws_vpc.main.cidr_block,8,1)"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-central-1a"
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "cidrsubnet(aws_vpc.main.cidr_block,8,2)"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-central-1b"
+}
+
 resource "aws_ecs_cluster" "this" {
   name = "app-cluster-challenge"
 }
