fd4c425742ad1a77e1fd8a10ce391e86d3e57f27
Hansel <hansel.hir@gmail.com>
2024-05-29 23:09:00 -0500

    new repository name

--- main.tf
+++ main.tf
@@ -42,14 +42,14 @@ provider "aws" {
 # DATA
 #############################################################################
 
-data "aws_ecr_repository" "this" {
-  name = var.ecr_repository
-}
+#data "aws_ecr_repository" "this" {
+#  name = var.ecr_repository
+#}
 
-data "aws_ecr_image" "this" {
-  repository_url = data.aws_ecr_repository.this.repository_url
-  tag = var.image_tag
-}
+#data "aws_ecr_image" "this" {
+#  repository_name  = data.aws_ecr_repository.this.name
+#  image_id         = data.aws_ecr_image_version.this.image_id
+#}
 
 #############################################################################
 # RESOURCES
@@ -65,7 +65,7 @@ resource "aws_ecs_task_definition" "this" {
     [
       {
         "name": "app",
-        "imageUri": "${data.aws_ecr_image.this.repository_url}:${data.aws_ecr_image.this.tag}",
+        "imageUri": "${aws_ecr_repository.example.repository_url}:0.0.1-SNAPSHOT",
         "portMappings": [
           {
             "containerPort": 8080,
@@ -82,6 +82,10 @@ resource "aws_ecs_task_definition" "this" {
     operating_system_family = "LINUX"
     cpu_architecture        = "ARM64"
   }
+}
+
+resource "aws_ecr_repository" "example" {
+  name = "hansel"
 }
 
 resource "aws_ecs_service" "this" {
