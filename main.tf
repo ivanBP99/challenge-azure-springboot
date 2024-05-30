451cd1d52383beff6f7bea3af66731e9047ac1d4
Hansel <hansel.hir@gmail.com>
2024-05-30 01:59:15 -0500

    route table and internet

--- main.tf
+++ main.tf
@@ -62,6 +62,7 @@ provider "aws" {
 #############################################################################
 
 resource "aws_vpc" "main" {
+  name = "vpc-ecr-two-subnet"
   cidr_block = var.vpc_cidr
   enable_dns_hostnames = true
   tags = {
@@ -81,6 +82,31 @@ resource "aws_subnet" "subnet2" {
   cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
   map_public_ip_on_launch = true
   availability_zone = "us-east-2b"
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    name = "internet_gateway"
+  }
+}
+
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "subnet_route" {
+  subnet_id = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet2_route" {
+  subnet_id = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.route_table.id
 }
 
 resource "aws_ecs_cluster" "this" {
