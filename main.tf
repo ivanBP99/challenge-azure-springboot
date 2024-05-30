2938d9f2761de7c6ef436596148fde2ef95d0334
Hansel <hansel.hir@gmail.com>
2024-05-30 01:03:18 -0500

    type string rollback

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ variable "image_tag" {
 
 variable "vpc_cidr" {
   description = "CIDR block for main"
-  type = String
+  type = string
   default = "10.0.0.0/16"
 }
 #############################################################################
