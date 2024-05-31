324b82b53f607188c42fe20fd890e37a6a59ba0e
Hansel <hansel.hir@gmail.com>
2024-05-31 02:34:43 -0500

    remove profile iam

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "aws_security_group" "lb_sg" {
 //abl
 
 resource "aws_lb" "ecs_alb" {
-  name               = "ecs-alb5"
+  name               = "ecs-alb6"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
@@ -144,7 +144,7 @@ resource "aws_lb_listener" "ecs_listener" {
 }
 
 resource "aws_lb_target_group" "ecs_tg" {
-  name        = "lb-target-group5"
+  name        = "lb-target-group6"
   port        = 80
   protocol    = "HTTP"
   target_type = "ip"
@@ -158,7 +158,7 @@ resource "aws_lb_target_group" "ecs_tg" {
 //cluster
 
 resource "aws_ecs_cluster" "cluster_challenge" {
-  name = "cluster-challenge"
+  name = "cluster-challenge1"
 }
 
 #resource "aws_ecs_cluster_capacity_providers" "cluster_provider" {
@@ -176,15 +176,17 @@ resource "aws_ecs_cluster" "cluster_challenge" {
 //template
 
 resource "aws_launch_template" "ecs_lt" {
-  name                   = "ecs-template-lt3"
+  name                   = "ecs-template-lt4"
   image_id               = "ami-09040d770ffe2224f"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.lb_sg.id]
 
-  iam_instance_profile {
-    name = "ecsInstanceRole"
-    # name = "testChallenge"
-  }
+  #iam_instance_profile {
+  #  name = "ecsInstanceRole"
+  #  name = "testChallenge"
+  #}
+
+
   monitoring {
     enabled = true
   }
