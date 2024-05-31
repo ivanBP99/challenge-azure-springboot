b8cd455b12f42466af65554c0f1a8b812e642eb1
Hansel <hansel.hir@gmail.com>
2024-05-31 00:08:32 -0500

    change name load balancer

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "aws_security_group" "lb_sg" {
 //abl
 
 resource "aws_lb" "ecs_alb" {
-  name               = "ecs-alb2"
+  name               = "ecs-alb1"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
@@ -144,7 +144,7 @@ resource "aws_lb_listener" "ecs_listener" {
 }
 
 resource "aws_lb_target_group" "ecs_tg" {
-  name        = "lb-target-group2"
+  name        = "lb-target-group1"
   port        = 80
   protocol    = "HTTP"
   target_type = "ip"
@@ -176,7 +176,7 @@ resource "aws_ecs_cluster" "cluster_challenge" {
 //template
 
 resource "aws_launch_template" "ecs_lt" {
-  name                   = "ecs-template"
+  name                   = "ecs-template-lt"
   image_id               = "ami-062c116e449466e7f"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.lb_sg.id]
