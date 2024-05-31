2a1e01e2aef12416ca4fbf2b2bdc65322578b9c3
Hansel <hansel.hir@gmail.com>
2024-05-31 03:32:40 -0500

    trigger plantimestamp

--- main.tf
+++ main.tf
@@ -123,7 +123,7 @@ resource "aws_security_group" "lb_sg" {
 //abl
 
 resource "aws_lb" "ecs_alb" {
-  name               = "ecs-alb7"
+  name               = "ecs-alb8"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
@@ -144,7 +144,7 @@ resource "aws_lb_listener" "ecs_listener" {
 }
 
 resource "aws_lb_target_group" "ecs_tg" {
-  name        = "lb-target-group7"
+  name        = "lb-target-group8"
   port        = 80
   protocol    = "HTTP"
   target_type = "ip"
@@ -158,7 +158,7 @@ resource "aws_lb_target_group" "ecs_tg" {
 //cluster
 
 resource "aws_ecs_cluster" "cluster_challenge" {
-  name = "cluster-challenge2"
+  name = "cluster-challenge3"
 }
 
 #resource "aws_ecs_cluster_capacity_providers" "cluster_provider" {
@@ -176,7 +176,7 @@ resource "aws_ecs_cluster" "cluster_challenge" {
 //template
 
 resource "aws_launch_template" "ecs_lt" {
-  name                   = "ecs-template-lt5"
+  name                   = "ecs-template-lt6"
   image_id               = "ami-09040d770ffe2224f"
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.lb_sg.id]
@@ -290,16 +290,17 @@ resource "aws_ecs_service" "ecs_service" {
   name            = "ecs-service"
   cluster         = aws_ecs_cluster.cluster_challenge.id
   task_definition = aws_ecs_task_definition.ecs_task_definition.arn
-  desired_count   = 1
+  desired_count   = 2
 #  launch_type     = "FARGATE"
 
-  force_new_deployment = true
+
   placement_constraints {
     type = "distinctInstance"
   }
+  force_new_deployment = true
 
   triggers = {
-    redeployment = timestamp()
+    redeployment = plantimestamp()
   }
 
   capacity_provider_strategy {
